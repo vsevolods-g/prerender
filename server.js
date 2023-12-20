b9e23547c62358167a78067e24820c6261e93eb5
Salem Hatoum <salem.hatoum@scandiweb.com>
2023-12-20 10:34:43 +0200

    Add Google auth and user-specific cache

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const parseSitemap = require('./lib/utils/parse-sitemap');
 const { typeDefs } = require('./lib/server/graphql/schema');
 const { resolvers } = require('./lib/server/graphql/resolvers');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
@@ -14,125 +15,88 @@ const port = process.env['EXPRESS_SERVER_PORT']; // Set the port number you want
 let browser;
 let mongo;
 
-const checkCache = async (req, res, next) => {
-    const { query: { url } } = req;
+function getUserFromRequest(req) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return null;
 
-    if (!url) {
-        res.send('Missing url param');
-
-        return;
-    }
-
-    const cache = await mongo.getCache(url);
-
-    if (cache) {
-        const { status, content } = cache;
-
-        res.status(status).send(content);
-
-        return;
-    }
-
-    next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.userId;
+  } catch (error) {
+    console.error('JWT verification error:', error);
+    return null;
+  }
 }
 
-// Define a route
-app.get('/', checkCache, async (req, res) => {
-    const { query: { url }, headers: { 'user-agent': userAgent } } = req;
-
-    browser.setUserAgent(userAgent);
-    const page = await browser.openNewTab({ targetUrl: url });
-
-    const status = browser.pagesStatusCode[url] || 200;
-    const content = page.pageContent;
-
-    try {
-        await mongo.insertCache({ url, content, status });
-    } catch(e) {
-        console.log(e);
-    }
-
-    res.status(status).send(content);
-});
-
-app.get('/cache/delete', async (req, res) => {
-    const { query: { url } } = req;
-
-    try {
-        const { deletedCount } = await mongo.deleteCache(url);
-
-        if (!deletedCount) {
-            res.status(404).send(`Cache for url: "${url}" doesn't exists.`)
-
-            return;
-        }
-
-        res.status(200).send(`Cache for url: "${url}" deleted successfully.`)
-    } catch(e) {
-        res.status(500).send(`Error while deleting cache for url: ${url}`);
-    }
-});
-
 app.get('/prerender/sitemap', async (req, res) => {
-    const { query: { path }, headers: { 'user-agent': userAgent } } = req;
+  const {
+    query: { path },
+    headers: { 'user-agent': userAgent },
+  } = req;
 
-    browser.setUserAgent(userAgent);
+  browser.setUserAgent(userAgent);
 
-    const parsedSitemap = await parseSitemap(path);
+  const parsedSitemap = await parseSitemap(path);
 
-    const preparedPromises = parsedSitemap.reduce((acc, urlArray) => {
-        const promisesArray = [];
+  const preparedPromises = parsedSitemap.reduce((acc, urlArray) => {
+    const promisesArray = [];
 
-        urlArray.map((url) => {
-            promisesArray.push(() => getSiteMapUrl(url));
-        })
+    urlArray.map((url) => {
+      promisesArray.push(() => getSiteMapUrl(url));
+    });
 
-        acc.push(promisesArray);
+    acc.push(promisesArray);
 
-        return acc;
-    }, [])
+    return acc;
+  }, []);
 
+  for (const arr of preparedPromises) {
+    await Promise.all(arr.map((fn) => fn()));
+  }
 
-    for (const arr of preparedPromises) {
-        await Promise.all(arr.map(fn => fn()));
-    }
-
-
-    res.status(200).send('Sitemap prerender finished');
+  res.status(200).send('Sitemap prerender finished');
 });
 
 async function getSiteMapUrl(url) {
-    const page = await browser.openNewTab({ targetUrl: url });
-    const status = browser.pagesStatusCode[url];
-    const content = page.pageContent;
+  const page = await browser.openNewTab({ targetUrl: url });
+  const status = browser.pagesStatusCode[url];
+  const content = page.pageContent;
 
-    await mongo.insertCache({ url, content, status });
+  await mongo.insertCache({ url, content, status });
 }
 
 async function startServer() {
-    const apolloServer = new ApolloServer({ typeDefs, resolvers });
-    await apolloServer.start();
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      const userId = getUserFromRequest(req);
+      const userAgent = req.headers['user-agent'];
+      return { userId, userAgent };
+    },
+  });
+  await apolloServer.start();
 
-    apolloServer.applyMiddleware({ app, path: '/graphql' });
+  apolloServer.applyMiddleware({ app, path: '/graphql' });
 }
 
 startServer();
 
 // Start the server
 app.listen(port, async () => {
-    if (!browser) {
-        browser = new Chrome();
+  if (!browser) {
+    browser = new Chrome();
 
-        await browser.startBrowser();
-        await browser.connectDevTools();
-    }
+    await browser.startBrowser();
+    await browser.connectDevTools();
+  }
 
-    if (!mongo) {
-        mongo = new MongoDBClient()
+  if (!mongo) {
+    mongo = new MongoDBClient();
 
-        await mongo.connect();
-        await mongo.createTTLIndex();
-    }
+    await mongo.connect();
+    await mongo.createTTLIndex();
+  }
 
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
